6b9d5805aba4a1d511775f2b84defa8449d1a3a0
paunin <d.m.paunin@gmail.com>
2014-10-18 16:03:51 +0400

    apigility

--- public/index.php
+++ public/index.php
@@ -1,3 +1,25 @@
 <?php
-require('init.php');
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+if (!defined('APPLICATION_PATH')) {
+    define('APPLICATION_PATH', realpath(__DIR__ . '/../'));
+}
+$appConfig = include APPLICATION_PATH . '/config/application.config.php';
+if (file_exists(APPLICATION_PATH . '/config/development.config.php')) {
+    $appConfig = Zend\Stdlib\ArrayUtils::merge($appConfig, include APPLICATION_PATH . '/config/development.config.php');
+}
+// Run the application!
+$app = Zend\Mvc\Application::init($appConfig);
 $app->run();
