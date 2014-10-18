d44d51f80b367bb483c7cb8ec450a457a9a60e87
paunin <d.m.paunin@gmail.com>
2014-10-18 23:02:20 +0400

    up

--- up.sh
+++ up.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
+git pull
 git submodule update --init --recursive
 chmod -R 777 data/cache
 composer update
-read -p "Press [Enter] key to start edit config files..."
 
-
-for filename in `find ./config/ ./module/ -name *.php.dist`
+firstfile=true
+for filename in `find ./config/ ./module/ -name *local.php.dist`
 do
     newfile=${filename:0:${#filename}-5}
-    echo " > Copied $filename to $newfile"
-    vim "$newfile";
+
+    if [ ! -f "$newfile" ]; then
+        if [ $firstfile = true ]; then
+            read -p " > Press [Enter] key to start searching end editing config files, OR [Ctrl+C] for do it later manually:"
+            firstfile=false
+        fi
+        read -p "    > Config in $filename now installed in $newfile. Press [Enter] key to edit it:"
+
+        cp "$filename" "$newfile"
+        vi "$newfile";
+    fi
 done
 
-echo 'Zf2Boilerplate installed!'
+echo ' > Zf2Boilerplate up-to-date!'
