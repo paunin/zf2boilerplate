afb0f8dba05ed6e3d5ab6bb641de41e6a8830887
paunin <d.m.paunin@gmail.com>
2014-10-18 17:00:14 +0400

    installer

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-git submodule update --init --recursive
+#git submodule update --init --recursive
+#chmod -R 777 data/cache
+#composer update
+read -p "Press [Enter] key to start edit config files..."
 
-chmod -R 777 data/cache
-composer update
 
-read -p "Press [Enter] key to start edit config file..."
-vim protected/config/custom.php
+for filename in `find ./config/ ./module/ -name *.php.dist`
+do
+    newfile=${filename:0:${#filename}-5}
+    echo " > Copied $filename to $newfile"
+    vim "$newfile";
+done
 
 echo 'Zf2Boilerplate installed!'
