271c4dadd29acc034845f51c8353ada2665250bb
paunin <d.m.paunin@gmail.com>
2014-10-18 17:09:07 +0400

    up

--- up.sh
+++ up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#git submodule update --init --recursive
-#chmod -R 777 data/cache
-#composer update
+git submodule update --init --recursive
+chmod -R 777 data/cache
+composer update
 read -p "Press [Enter] key to start edit config files..."
 
 
